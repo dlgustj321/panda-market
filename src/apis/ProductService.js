92968a2df08d29238a5068c6050b740dae6532b3
이현서 <lhs103725@gmail.com>
2024-12-05 03:35:03 +0900

    최종 수정

--- src/apis/ProductService.js
+++ src/apis/ProductService.js
@@ -1,25 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:5500",
-  // baseURL: "https://four-sprint-mission-fe-1.onrender.com",
+  baseURL: 'http://localhost:5500',
+  //baseURL: 'https://four-sprint-mission-be.onrender.com',
   timeout: 3000,
 });
 
-export const getProducts = async ({
-  sort = "recent",
-  offset = 0,
-  keyword = "",
-  limit = 0,
-}) => {
+export const getProducts = async ({ sort = 'recent', offset = 0, keyword = '', limit = 0 }) => {
   const query = `sort=${sort}&offset=${offset}&keyword=${keyword}&limit=${limit}`;
   const res = await instance.get(`/products?${query}`);
   return res.data;
 };
 
 export const createProduct = async (productData) => {
-  const res = await instance.post("/products", productData);
+  const res = await instance.post('/products', productData);
   // eslint-disable-next-line no-restricted-globals
-  location.href("/");
+  location.href('/');
   return res.data;
 };
